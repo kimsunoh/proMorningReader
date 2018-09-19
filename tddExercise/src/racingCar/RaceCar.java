cfac2a922a95abeed77cdaeff29581d319fa7fd0
Rudia Moon <impactyoung@gmail.com>
2018-09-20 07:53:15 +0900

    testcase 작업내역
    - 전진값을 주어졌을떄 자동차의 현재 위치값이 변경됨
    - 멈춤값을 주어졌을떄 자동차의 현재 위치값이 변경되지 않음
    - 전진조건값이 4이상일때 전진값이 맞게 주어지는지 확인
    - 전진조건값이 4미만일때 전진값이 맞게 주어지는지 확인

--- tddExercise/src/racingCar/RaceCar.java
+++ tddExercise/src/racingCar/RaceCar.java
@@ -1,6 +1,26 @@
 package racingCar;
 
 public class RaceCar {
+	int coord = 0;
+	
+	public int getCoord() {
+		return coord;
+	}
+
+	public void moveCar() {
+		this.coord +=1;
+	}
+
+	public void stopCar() {
+		this.coord = this.coord;//TODO
+	}
+
+	public int goCarValue(int randomResult) {
+		if(randomResult>=4)
+			return 1;
+		return 0;
+	}
+
 
 
 //    public int inputCarNum(int inputCarNum) {
--- tddExercise/test/racingCar/RaceCarTest.java
+++ tddExercise/test/racingCar/RaceCarTest.java
@@ -22,4 +22,33 @@ public class RaceCarTest {
         RacingGame racingGame = new RacingGame(3);
         assertEquals(inputCarNum, racingGame.rc.size());
     }
+    
+    @Test
+    public void 전진값을_주어졌을떄_자동차의_현재_위치값이_변경됨() {
+    	RaceCar raceCar = new RaceCar();
+    	int cuurrentCoord = raceCar.getCoord();
+    	raceCar.moveCar();
+    	assertEquals(cuurrentCoord+1, raceCar.getCoord());
+    }
+    
+    @Test
+    public void 멈춤값을_주어졌을떄_자동차의_현재_위치값이_변경되지_않음() {
+    	RaceCar raceCar = new RaceCar();
+    	int cuurrentCoord = raceCar.getCoord();
+    	assertEquals(cuurrentCoord, raceCar.getCoord());
+    }
+    
+    @Test
+    public void 전진조건값이_4이상일때_전진값이_맞게_주어지는지_확인() {
+    	RaceCar raceCar = new RaceCar();
+    	int randomResult = 4;
+    	assertEquals(1, raceCar.goCarValue(randomResult));
+    }
+    
+    @Test
+    public void 전진조건값이_4미만일때_전진값이_맞게_주어지는지_확인() {
+    	RaceCar raceCar = new RaceCar();
+//    	int randomResult = 3;
+    	assertEquals(0, 1);
+    }
 }
